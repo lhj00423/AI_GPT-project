0eebcba8ceffe73f4e30aad182898eee3446f148
LHY1004 <lhj100411@naver.com>
2023-09-11 18:38:38 +0900

    230911

---
+++ script.js
@@ -0,0 +1,6 @@
+const generateBtn = document.querySelector('#generateBtn')
+
+generateBtn.addEventListener('click',  () => {
+    // 무언가 하겠다! --> 클릭 이벤트가 발생했을때 실행할 코드
+    console.log('버튼을 누르면 진짜 이 함수가 실행되나?');
+})
