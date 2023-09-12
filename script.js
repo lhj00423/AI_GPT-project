f6daa3fe9aa07d9338a1dbd04351889b546162b7
LHY1004 <lhj100411@naver.com>
2023-09-12 15:33:25 +0900

    230912

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const generateBtn = document.querySelector('#generateBtn')
+const generateBtn = document.querySelector('#generateBtn'); // 1
+const gridContainer = document.querySelector('#gridContainer'); // 2
 
-generateBtn.addEventListener('click',  () => {
-    // 무언가 하겠다! --> 클릭 이벤트가 발생했을때 실행할 코드
-    console.log('버튼을 누르면 진짜 이 함수가 실행되나?');
-})
+generateBtn.addEventListener('click', () => { //3
+    const randomNumber = Math.floor(Math.random() * 1000) +1; //4
+    const imgUrl = `https://picsum.photos/500?random=${randomNumber}`; //5
+
+    const img = document.createElement('img'); // 6
+    //img.src = imgUrl; //7 -> 4+5 결합된 코드 
+    img.src = `https://picsum.photos/500?random=${Math.floor(Math.random() * 1000) +1}`
+    console.log(Math.floor(Math.random() * 1000) +1);
+    // ${} -> 어떤 문자열 안에 동적인 값을 넣을떄 쓰는 문법 
+    gridContainer.appendChild(img); //8
+});
